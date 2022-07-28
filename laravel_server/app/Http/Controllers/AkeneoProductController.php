e242695889699b81feceb4fb4f14fdc014439598
Jars <anon>
2022-07-28 22:16:22 +0200

    generated akeneoproduct model

---
+++ laravel_server/app/Http/Controllers/AkeneoProductController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreAkeneoProductRequest;
+use App\Http\Requests\UpdateAkeneoProductRequest;
+use App\Models\AkeneoProduct;
+
+class AkeneoProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreAkeneoProductRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreAkeneoProductRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\AkeneoProduct  $akeneoProduct
+     * @return \Illuminate\Http\Response
+     */
+    public function show(AkeneoProduct $akeneoProduct)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\AkeneoProduct  $akeneoProduct
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(AkeneoProduct $akeneoProduct)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateAkeneoProductRequest  $request
+     * @param  \App\Models\AkeneoProduct  $akeneoProduct
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateAkeneoProductRequest $request, AkeneoProduct $akeneoProduct)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\AkeneoProduct  $akeneoProduct
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(AkeneoProduct $akeneoProduct)
+    {
+        //
+    }
+}
